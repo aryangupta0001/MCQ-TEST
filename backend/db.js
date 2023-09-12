755ec81670a0ace476dbf20b01c9219919235ecb
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-09-13 01:41:59 +0530

    database connection

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const mongoose_url = "mongodb://127.0.0.1/mcqtest"
+
+const connectToMongo = async () => {
+    await mongoose.connect(mongoose_url)
+    const dbConnect = mongoose.connection;          // Connection object
+
+    console.log("Connected To Mongo Successfully");
+}
+
+module.exports = connectToMongo;
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const connectToMongo = require("./db");
+const express = require('express');
+const cors = require("cors");
+
+
+
+connectToMongo();
+
+const PORT = 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());        // Use this middleware to parse incoming requests with JSON Payload.
+
+// Available Routes :-
+app.use("/api/exam/", require("./routes/exam"));
+// app.use("/api/notes/", require("./routes/notes"));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on PORT ${PORT}`);
+  console.log(`Link :- http://localhost:${PORT}`);
+})
